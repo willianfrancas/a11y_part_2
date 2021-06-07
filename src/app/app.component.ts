05c84abaed3e50da82eae13e803e51529812bef9
willianfrancas <willianfrancas@gmail.com>
2021-06-07 20:52:05 -0300

    first commit with module_01

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  @ViewChild('modal') modalTemplateRef: TemplateRef<any>;
+  title = 'a11y-p2';
+  public firstName = 'William';
+  public modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  showModal() {
+    this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: ''
+    });
+  }
+}
---
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, TemplateRef } from "@angular/core";
+
+@Injectable()
+export class ModalService {
+
+
+  public open(modalConfig: ModalConfig): ModalRef {
+    console.log('open called');
+    return new ModalRef();
+  }
+}
+
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>,
+  title: string,
+}
+
+export class ModalRef {
+
+  public close(): void {
+    console.log('closed called');
+
+  }
+}
